faeae346bd6710626e5ba1d12f726b834608a56d
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-09-23 21:16:22 +0200

    Minor fixes (typos etc.)

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const aside = document.querySelector("aside");
 const submitBtn = document.querySelector("submit");
 const newBookBtn = document.createElement("button");
 newBookBtn.setAttribute("id", "newBookBtn");
-newBookBtn.innerHTML = "<i class='fa-solid fa-circle-plus'></i>Add book";
+newBookBtn.innerHTML = "<i class='fa-solid fa-circle-plus'></i> Add book";
 aside.appendChild(newBookBtn);
 
 function Book(title, author, pages, read) {
@@ -37,14 +37,6 @@ const cross = document.getElementById("cross");
 cross.addEventListener("click", () => {
 	dialog.close();
 });
-/*
-let pagesInput = document.getElementById("pages").value;
-pagesInput.onchange = checkNumber;
-
-function checkNumber() {
-	const numbersRegex = /\d/gim;
-	if(numbersRegex.test(pagesInput))
-}*/
 
 function onFormSubmit(event) {
 	let content = document.querySelector("content");
@@ -121,8 +113,8 @@ let theNeuromancer = new Book(
 	"not read yet",
 );
 
-addBookToLibrary(theHobbit);
 addBookToLibrary(theNeuromancer);
+addBookToLibrary(theHobbit);
 myLibrary.map(createCard);
 
 function removeBookFromLibrary(index) {
@@ -131,122 +123,3 @@ function removeBookFromLibrary(index) {
 	myLibrary.splice(index, 1);
 	myLibrary.map(createCard);
 }
-
-/*
-const main = document.querySelector("main");
-const bookDialog = document.createElement("dialog");
-const bookForm = document.createElement("form");
-const titleLabel = document.createElement("label");
-const titleInput = document.createElement("input");
-titleLabel.textContent = "Title";
-titleLabel.setAttribute("for", "title");
-titleInput.setAttribute("id", "title");
-titleInput.setAttribute("type", "text");
-titleInput.setAttribute("minlength", "1");
-titleInput.required = true;
-
-const authorLabel = document.createElement("label");
-const authorInput = document.createElement("input");
-authorLabel.textContent = "Author";
-authorLabel.setAttribute("for", "author");
-authorInput.setAttribute("id", "author");
-authorInput.setAttribute("type", "text");
-authorInput.setAttribute("minlength", "1");
-authorInput.required = true;
-
-const pagesLabel = document.createElement("label");
-const pagesInput = document.createElement("input");
-pagesLabel.textContent = "No. of pages";
-pagesLabel.setAttribute("for", "pages");
-pagesInput.setAttribute("id", "pages");
-pagesInput.setAttribute("type", "text");
-pagesInput.setAttribute("min", "1");
-pagesInput.required = true;
-
-const radiobuttonFieldset = document.createElement("fieldset");
-const readLabel = document.createElement("label");
-readLabel.textContent = "Read";
-const readInput = document.createElement("input");
-readInput.setAttribute("type", "radio");
-readInput.setAttribute("id", "read");
-readInput.setAttribute("name", "readStatus");
-readInput.setAttribute("value", "read");
-
-const notReadLabel = document.createElement("label");
-const notReadInput = document.createElement("input");
-notReadLabel.textContent = "Not read yet";
-notReadInput.setAttribute("type", "radio");
-notReadInput.setAttribute("id", "notReadYet");
-notReadInput.setAttribute("name", "readStatus");
-notReadInput.setAttribute("value", "not read yet");
-
-const submitBtn = document.createElement("input");
-submitBtn.setAttribute("id", "submit");
-submitBtn.setAttribute("type", "submit");
-submitBtn.setAttribute("value", "Add book");
-
-bookDialog.appendChild(bookForm);
-bookForm.appendChild(titleLabel);
-bookForm.appendChild(titleInput);
-
-bookForm.appendChild(authorLabel);
-bookForm.appendChild(authorInput);
-
-bookForm.appendChild(pagesLabel);
-bookForm.appendChild(pagesInput);
-
-bookForm.appendChild(radiobuttonFieldset);
-radiobuttonFieldset.appendChild(readLabel);
-radiobuttonFieldset.appendChild(notReadLabel);
-readLabel.appendChild(readInput);
-notReadLabel.appendChild(notReadInput);
-main.appendChild(bookDialog);
-
-newBookBtn.addEventListener("click", () => {
-	bookDialog.showModal();
-});
-
-/*
-
-
-
-/*
-function validateInput() {
-	let titleInput = document.getElementById("title");
-	let titleValidity = titleInput.checkValidity();
-	let authorInput = document.getElementById("author").textContent;
-	let authorValidity = authorInput.checkValidity();
-	let pagesInput = document.getElementById("pages");
-	let pagesValidity = pagesInput.checkValidity();
-	if (titleValidity && authorValidity && pagesValidity) {
-		return true;
-	} else {
-		if (!titleValidity) {
-			titleInput.innerHTML = titleInput.validationMessage;
-		}
-		if (!authorValidity) {
-			authorInput.innerHTML = authorInput.validationMessage;
-		}
-		if (!pagesValidity) {
-			pagesInput.innerHTML = pagesInput.validationMessage;
-		}
-	}
-}
-*/
-/*
-
-
-	let readBtns = document.querySelectorAll(".readBtn");
-	for (btn of readBtns) {
-		btn.addEventListener("click", function () {
-			let index = Number(btn.name);
-			myLibrary[index].toggleRead();
-			showTable();
-			addColumnHeaderRemove();
-			addRemoveButtons();
-			addColumnHeaderRead();
-			addReadButtons();
-		});
-	}
-}
-*/
